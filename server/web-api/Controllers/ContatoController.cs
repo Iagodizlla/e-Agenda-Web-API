023b4aa760e06349b8fec17197ea0b895ddd7b54
Iagodizlla <iagoschlemper@gmail.com>
2025-09-05 19:30:08 -0300

    Implementada Seleção de Contatos por Id

--- server/web-api/Controllers/ContatoController.cs
+++ server/web-api/Controllers/ContatoController.cs
@@ -1,4 +1,3 @@
-using eAgenda.Core.Dominio.ModuloContato;
 using eAgenda.WebApi.Models.ModuloContato;
 using eAgenda.Core.Aplicacao.ModuloContato.Commands;
 using MediatR;
@@ -49,6 +48,29 @@ public class ContatoController(IMediator mediator) : ControllerBase
              result.Value.Contatos.Count,
              result.Value.Contatos
          );
+
+        return Ok(response);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<SelecionarContatosResponse>> SelecionarRegistroPorId(Guid id)
+    {
+        var query = new SelecionarContatoPorIdQuery(id);
+
+        var result = await mediator.Send(query);
+
+        if (result.IsFailed)
+            return NotFound(id);
+
+        var response = new SelecionarContatoPorIdResponse(
+            result.Value.Id,
+            result.Value.Nome,
+            result.Value.Telefone,
+            result.Value.Email,
+            result.Value.Empresa,
+            result.Value.Cargo,
+            result.Value.Compromissos
+        );
 
         return Ok(response);
     }
